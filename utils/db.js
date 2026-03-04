1a95663ea6afa3349c0f900983f80f1999f438dc
gonzalopriv9-byte <gonzalo.priv9@gmail.com>
2026-03-04 18:03:22 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
 
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
+  throw new Error("Faltan SUPABASE_URL o SUPABASE_KEY en el archivo .env");
+}
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
